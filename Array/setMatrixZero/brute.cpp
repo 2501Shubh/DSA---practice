a68a6e22724ba1f3239d3dee3518914c4b7e5355
2501Shubh <153253024+2501Shubh@users.noreply.github.com>
2024-05-17 11:57:51 +0000

    Set Matrix Zero: Brute

---
+++ Array/setMatrixZero/brute.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+
+//set that entire row as -1
+void setRow(int i, vector<vector<int>> &matrix, int m)
+{
+	for(int j=0; j<m; j++)
+	{
+		if(matrix[i][j] != 0)
+		{
+			matrix[i][j] = -1;
+		}
+	}
+}
+
+//set that entire column as -1
+void setColumn(int j, vector<vector<int>> &matrix, int n)
+{
+	for(int i=0; i<n; i++)
+	{
+		if(matrix[i][j] != 0)
+		{
+			matrix[i][j] = -1;
+		}
+	}
+}
+
+vector<vector<int>> zeroMatrix(vector<vector<int>> &matrix, int n, int m) {
+	/*
+		n: number of rows
+		m: number of columns
+	*/
+
+	//check the intial 0's
+	for(int i=0; i<n; i++)
+	{
+		for(int j=0; j<m; j++)
+		{
+			if(matrix[i][j] == 0)
+			{
+				setRow(i, matrix, m);
+				setColumn(j, matrix, n);
+			}
+		}
+	}
+
+	for(int i=0; i<n; i++)
+	{
+		for(int j=0; j<m; j++)
+		{
+			if(matrix[i][j] == -1)
+			{
+				matrix[i][j] = 0;
+			}
+		}
+	}
+	return matrix;
+}
